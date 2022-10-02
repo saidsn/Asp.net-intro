2eff56550d27e61209b360788bae8faf37f92e51
saidsn <saidsn@code.edu.az>
2022-10-02 20:32:24 +0400

    Asp-Homework

---
+++ Asp-ClassCode/Asp-Intro/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index(int id, string name)
+        //{
+        //    return "Home Page" + "-" + id + "-" + name;
+        //}
+
+        //public IActionResult Index(int id, string name)
+        //{
+        //    return Json(new { name = "Seid", surname = "Nuraliyev" });
+        //}
+
+        //public IActionResult Index(int id, string name)
+        //{
+        //    return Content(9.ToString());
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string FullData(string name,string surname,int age)
+        //{
+        //    return $"Name-{name}: Surname-{surname}: Age-{age}";
+        //}
+    }
+}
